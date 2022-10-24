31321ad4cef355b495e894b8665f9d2c049f456a
lizhi.guo <lizhi.guo@xjsd.com>
2022-10-24 14:15:50 +0800

    feat: add px2vw support

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+/* eslint-disable import/no-extraneous-dependencies */
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import px2vw from 'postcss-px-to-viewport';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [vue()],
+  css: {
+    postcss: {
+      plugins: [
+        px2vw({
+          unitToConvert: 'px',
+          viewportWidth: 750,
+          unitPrecision: 6,
+          propList: ['*'],
+          viewportUnit: 'vw',
+          fontViewportUnit: 'vw',
+          selectorBlackList: ['ignore-'],
+          minPixelValue: 1,
+          mediaQuery: true,
+          replace: true,
+          exclude: [],
+          landscape: false,
+        }),
+      ],
+    },
+  },
+});
